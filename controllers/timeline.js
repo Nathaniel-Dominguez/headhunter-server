4781715d358ff619300ae605f4a982c30625c466
billyhinman712 <41489170+billyhinman712@users.noreply.github.com>
2018-09-13 14:32:20 -0700

    created delete route

--- controllers/timeline.js
+++ controllers/timeline.js
@@ -4,18 +4,41 @@ const mongoose = require('mongoose');
 const db = require('../models');
 const router = express.Router();
 
+//pull data from data base for timeline
+
 //post route for array of oblects for timeline
 router.post('/', (req, res) => {
 	console.log(req.body);
 
-	req.body.forEach((e) => {
-		db.Task.create(e)
+	req.body.forEach((element) => {
+		db.Task.create(element)
     .then((createdTask) => {
-      console.log('created task', createdTask);
+      console.log('created task:', createdTask);
     })
     .catch((err) => {
       console.log('err', err);
       res.status(500).send('Could not create task in DB');
     });
-  res.send({ tasks: req.body });
+});
+
+//edit tasks
+router.put('/', (req, res) => {
+
+})
+
+//delete tasks
+router.delete('/', (req, res) => {
+	db.Task.findOne({
+		where: {id: req.params.id}
+	}).then(() => {
+		db.tag.destroy({
+				where: {id: req.params.id}
+			}).then(() => {
+				res.send('success');
+			}).catch(err => {
+				res.status(500).send('failed to delete!');
+			});
+		}).catch(err => {
+			res.status(500).send('failed to find task!');
+		});
 });
