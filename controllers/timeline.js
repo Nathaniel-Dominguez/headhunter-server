36665c38c4c24fb2a1a74bc24ce3582f3cca252f
billyhinman712 <41489170+billyhinman712@users.noreply.github.com>
2018-09-13 14:53:41 -0700

    changed delete and edit routes for mongoose

--- controllers/timeline.js
+++ controllers/timeline.js
@@ -5,6 +5,7 @@ const db = require('../models');
 const router = express.Router();
 
 //pull data from data base for timeline
+//HOW DO I DO THISS???????
 
 //post route for array of oblects for timeline
 router.post('/', (req, res) => {
@@ -23,22 +24,39 @@ router.post('/', (req, res) => {
 
 //edit tasks
 router.put('/', (req, res) => {
-
-})
+	// This would likely be inside of a PUT request, since we're updating an existing document, hence the req.params.todoId.
+	// Find the existing resource by ID
+	db.Task.findByIdAndUpdate(
+    // the id of the item to find
+    req.params.taskId,
+    
+    // the change to be made. Mongoose will smartly combine your existing 
+    // document with this change, which allows for partial updates too
+    req.body,
+    
+    // an option that asks mongoose to return the updated version 
+    // of the document instead of the pre-updated one.
+    {new: true},
+    
+    // the callback function
+    (err, todo) => {
+    // Handle any possible database errors
+        if (err) return res.status(500).send(err);
+        return res.send(todo);
+    });
+});
 
 //delete tasks
 router.delete('/', (req, res) => {
-	db.Task.findOne({
-		where: {id: req.params.id}
-	}).then(() => {
-		db.tag.destroy({
-				where: {id: req.params.id}
-			}).then(() => {
-				res.send('success');
-			}).catch(err => {
-				res.status(500).send('failed to delete!');
-			});
-		}).catch(err => {
-			res.status(500).send('failed to find task!');
-		});
+		db.Task.findByIdAndRemove(req.params.taskId, (err, task) => {
+    // As always, handle any potential errors:
+    if (err) return res.status(500).send(err);
+    // We'll create a simple object to send back with a message and the id of the document that was removed
+    // You can really do this however you want, though.
+    const response = {
+        message: "Todo successfully deleted",
+        id: task._id
+    };
+    return res.status(200).send(response);
+});
 });
